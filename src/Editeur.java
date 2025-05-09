f3b6ce2c38135b66286a23aedcbea99035bdcc9b
nathan2612 <nathan.joubert@eu.univ-orleans.fr>
2025-05-09 11:30:47 +0200

    ajout fichier test avec un premier test de la class Livre qui passe

--- src/Editeur.java
+++ src/Editeur.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Editeur {
@@ -5,10 +6,10 @@ public class Editeur {
     private String nomEditeur;
     private List<Livre> livres;
 
-    public Editeur(int idEditeur,String nomEditeur, List<Livre> livres){
+    public Editeur(int idEditeur,String nomEditeur){
         this.idEditeur = idEditeur;
         this.nomEditeur = nomEditeur;
-        this.livres = livres;
+        this.livres = new ArrayList<>();
     }
     
     public int idEditeur(){
