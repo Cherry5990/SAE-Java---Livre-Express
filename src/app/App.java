944372a7cc856d419e41d95820daccf23794174e
nathan2612 <nathan.joubert@eu.univ-orleans.fr>
2025-06-16 08:55:39 +0200

    z

--- src/app/App.java
+++ src/app/App.java
@@ -1,5 +1,16 @@
 package app;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
     
+    @Override
+    public void init(){
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.primaryStage = stage;
+    }
 }
