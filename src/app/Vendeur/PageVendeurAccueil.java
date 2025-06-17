9850f1d9b91d487aff9f0d082149b4c1a3a2cd7c
alexandre1dias <alexandre.dias1@etu.univ-orleans.fr>
2025-06-17 16:01:24 +0200

    ajout des classes java de PageVendeurAccueil et PageVendeurGererStocks

---
+++ src/app/Vendeur/PageVendeurAccueil.java
@@ -0,0 +1,69 @@
+package app.Vendeur;
+import modele.Vendeur;
+
+import java.io.IOException;
+
+import app.App;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class PageVendeurAccueil {
+    private Scene scene;
+    private Vendeur vendeur;
+
+    public PageVendeurAccueil(App app,Vendeur v)throws IOException{
+        this.vendeur = v;
+        Pane root = FXMLLoader.load(getClass().getResource("../view/Vendeur/PageVendeurAccueil.fxml"));
+        this.scene = new Scene(root);
+
+        Button deconnexion = (Button) this.scene.lookup("#deconnexion");
+        deconnexion.setOnMouseEntered(e -> {
+                deconnexion.setScaleX(1.1);
+                deconnexion.setScaleY(1.1);
+            });
+            deconnexion.setOnMouseExited(e -> {
+                deconnexion.setScaleX(1.0);
+                deconnexion.setScaleY(1.0);
+            });
+        deconnexion.setOnAction(e -> app.sceneAcceuil());
+
+        Button gererStock = (Button) this.scene.lookup("#gererStock");
+        gererStock.setOnMouseEntered(e -> {
+                gererStock.setScaleX(1.1);
+                gererStock.setScaleY(1.1);
+            });
+            gererStock.setOnMouseExited(e -> {
+                gererStock.setScaleX(1.0);
+                gererStock.setScaleY(1.0);
+            });
+        gererStock.setOnAction(e -> {
+            try {
+                app.scenePageVendeurGererStocks(this.vendeur);
+            } catch (IOException ex) {
+                System.out.println("Problème");
+            }
+        });
+
+        Button commande = (Button) this.scene.lookup("#commande");
+        commande.setOnMouseEntered(e -> {
+                commande.setScaleX(1.1);
+                commande.setScaleY(1.1);
+            });
+            commande.setOnMouseExited(e -> {
+                commande.setScaleX(1.0);
+                commande.setScaleY(1.0);
+            });
+        //gererStock.setOnAction(e -> app.scenePageVendeurCommande());
+
+
+
+    }
+
+    public Scene getScene(){
+        return this.scene;
+    }
+
+    
+}
