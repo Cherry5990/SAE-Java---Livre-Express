23f9737d01bcf34c94d351e2e6e1bdee45db3719
alexandre1dias <alexandre.dias1@etu.univ-orleans.fr>
2025-06-17 16:30:33 +0200

    debut du code de  PageVendeurAjouterLivre - scene à modifier avant de pouvoir finir

---
+++ src/app/Vendeur/PageVendeurAjouterLivre.java
@@ -0,0 +1,57 @@
+package app.Vendeur;
+
+import java.io.IOException;
+
+import app.App;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import modele.Vendeur;
+
+public class PageVendeurAjouterLivre {
+    private Scene scene;
+    private Vendeur vendeur;
+
+    //A finir
+    public PageVendeurAjouterLivre(App app, Vendeur v)throws IOException{
+        this.vendeur = v;
+        Pane root = FXMLLoader.load(getClass().getResource("../view/Vendeur/PageVendeurAjouterLivre.fxml"));
+        this.scene = new Scene(root);
+
+        Button deconnexion = (Button) this.scene.lookup("#deconnexion");
+        deconnexion.setOnMouseEntered(e -> {
+                deconnexion.setScaleX(1.1);
+                deconnexion.setScaleY(1.1);
+            });
+            deconnexion.setOnMouseExited(e -> {
+                deconnexion.setScaleX(1.0);
+                deconnexion.setScaleY(1.0);
+            });
+        deconnexion.setOnAction(e -> app.sceneAcceuil());
+
+        Button retour = (Button) this.scene.lookup("#retour");
+        retour.setOnMouseEntered(e -> {
+                retour.setScaleX(1.1);
+                retour.setScaleY(1.1);
+            });
+            retour.setOnMouseExited(e -> {
+                retour.setScaleX(1.0);
+                retour.setScaleY(1.0);
+            });
+        retour.setOnAction(e -> {
+            try {
+                app.scenePageVendeurGererStocks(v);
+            } catch (IOException ex) {
+                System.out.println("Problème");
+            }
+        });
+
+    
+
+    }
+
+    public Scene getScene(){
+        return this.scene;
+    }
+}
