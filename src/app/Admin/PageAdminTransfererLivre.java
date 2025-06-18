157bdec3607cd850b5c2478daceeb650ada69635
KuRika28 <erika.de-jesus-pinto-pereira@etu.univ-orleans.fr>
2025-06-18 09:41:35 +0200

    pages adminAccueil, GererStock faites

--- src/app/Admin/PageAdminTransfererLivre.java
+++ src/app/Admin/PageAdminTransfererLivre.java
@@ -1,7 +1,5 @@
 package app.Admin;
-import modele.Client;
 
-import java.io.IOError;
 import java.io.IOException;
 
 import app.App;
@@ -9,12 +7,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
-public class PageAdmin {
+public class PageAdminTransfererLivre {
     private Scene scene;
 
-    public PageAdmin(App app)throws IOException{
-        Pane root = FXMLLoader.load(getClass().getResource("view/PageClient.fxml"));
+    public PageAdminTransfererLivre(App app) throws IOException{
+        Pane root = FXMLLoader.load(getClass().getResource("../view/Admin/PageAdminTransfererLivre.fxml"));
         this.scene = new Scene(root);
+        
     }
 
     public Scene getScene(){
