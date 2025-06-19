dd147b9405d94681d43999f69d17e2a9d578beb4
nathan2612 <nathan.joubert@etu.univ-orleans.fr>
2025-06-19 22:44:18 +0200

    pagination des recommandé et des commandes

--- src/app/Client/PageClient.java
+++ src/app/Client/PageClient.java
@@ -17,6 +17,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Alert;
@@ -32,6 +33,8 @@ public class PageClient {
     private int positionCommande;
     private App app;
     private String magasinChoix;
+    private Label page1;
+    private Label page2;
 
     public PageClient(App app)throws IOException{
         Pane root = FXMLLoader.load(getClass().getResource("../view/Client/PageClientAccueil.fxml"));
@@ -55,28 +58,58 @@ public class PageClient {
             app.popUpMessageDeconnexion();
         });
 
+        this.page1 = (Label)scene.lookup("#page1");
+        this.page1.setText("Page "+(positionLivre/7+1)+"/"+(App.recoClient.get(App.client).size()/7+1));
+        this.page2 =(Label)scene.lookup("#page2");
+
         Button connecter = (Button) this.scene.lookup("#connexion");
         connecter.setOnAction(new ControleurConnexionMagasin(this,app));
         connecter.setOnMouseEntered(e -> connecter.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white; -fx-scale-x: 1.05; -fx-scale-y: 1.05;"));
         connecter.setOnMouseExited(e -> connecter.setStyle("-fx-background-color: a6897e;"));
 
         Button suivant = (Button) this.scene.lookup("#suivant");
-        suivant.setOnAction(new ControleurSuivant(this));
+        suivant.setOnAction(e -> {
+            if(getPositionLivre()+7<getNbLivre()){
+                setPositionLivre(getPositionLivre()+7);
+            }
+            majRecommandation();
+        });
         suivant.setOnMouseEntered(e -> suivant.setStyle("-fx-background-color: #2196F3; -fx-text-fill: white; -fx-scale-x: 1.08; -fx-scale-y: 1.08; -fx-effect: dropshadow(gaussian, #1976D2, 10, 0.5, 0, 2);"));
         suivant.setOnMouseExited(e -> suivant.setStyle("-fx-background-color: transparent;"));
 
         Button avant = (Button) this.scene.lookup("#avant");
-        avant.setOnAction(new ControleurAvant(this));
+        avant.setOnAction(e -> {
+            if(getPositionLivre()-7<0){
+                setPositionLivre(0);
+            }
+            else{
+                setPositionLivre(getPositionLivre()-7);
+            }
+            majRecommandation();
+        });
         avant.setOnMouseEntered(e -> avant.setStyle("-fx-background-color: #2196F3; -fx-text-fill: white; -fx-scale-x: 1.08; -fx-scale-y: 1.08; -fx-effect: dropshadow(gaussian, #1976D2, 10, 0.5, 0, 2);"));
         avant.setOnMouseExited(e -> avant.setStyle("-fx-background-color: transparent;"));
 
         Button suivant1 = (Button) this.scene.lookup("#suivant1");
-        suivant1.setOnAction(new ControleurSuivant1(this));
+        suivant1.setOnAction(e -> {
+            if(getPositionCommande()+7<getNbCommande()){
+                setPositionCommande(getPositionCommande()+7);
+            }
+            majCommande();
+        });
         suivant1.setOnMouseEntered(e -> suivant1.setStyle("-fx-background-color: #2196F3; -fx-text-fill: white; -fx-scale-x: 1.08; -fx-scale-y: 1.08; -fx-effect: dropshadow(gaussian, #1976D2, 10, 0.5, 0, 2);"));
         suivant1.setOnMouseExited(e -> suivant1.setStyle("-fx-background-color: transparent;"));
 
         Button avant1 = (Button) this.scene.lookup("#avant1");
-        avant1.setOnAction(new ControleurAvant1(this));
+        avant1.setOnAction(e -> {
+            if(getPositionCommande()-7<0){
+                setPositionCommande(0);
+            }
+            else{
+                setPositionCommande(getPositionCommande()-7);
+            }
+            majCommande();
+        });
         avant1.setOnMouseEntered(e -> avant1.setStyle("-fx-background-color: #2196F3; -fx-text-fill: white; -fx-scale-x: 1.08; -fx-scale-y: 1.08; -fx-effect: dropshadow(gaussian, #1976D2, 10, 0.5, 0, 2);"));
         avant1.setOnMouseExited(e -> avant1.setStyle("-fx-background-color: transparent;"));
 
@@ -96,6 +129,7 @@ public class PageClient {
         try{
             this.positionCommande=0;
             this.commandes= App.commandeBD.CommandeClient(App.client.getId());
+            this.page2.setText("Page "+(positionCommande/7+1)+"/"+(commandes.size()/7+1));
             this.hBoxCommande = (HBox) this.scene.lookup("#commandes");
             int count2 = Math.min(7, commandes.size());
             for (int i = 0; i < count2; i++) {
@@ -116,6 +150,7 @@ public class PageClient {
     }
 
     public void majRecommandation() {
+        this.page1.setText("Page "+(positionLivre/7+1)+"/"+(App.recoClient.get(App.client).size()/7+1));
         this.livres.getChildren().clear();
         int count = Math.min(7, App.recoClient.get(App.client).size() - positionLivre);
         for (int j = 0; j < count; j++) {
@@ -129,6 +164,7 @@ public class PageClient {
 
     public void majCommande() {
         this.hBoxCommande.getChildren().clear();
+        this.page2.setText("Page "+(positionCommande/7+1)+"/"+(commandes.size()/7+1));
         int count = Math.min(7, commandes.size() - positionCommande);
         for (int j = 0; j < count; j++) {
             Commande dataCommande = commandes.get(positionCommande + j);
